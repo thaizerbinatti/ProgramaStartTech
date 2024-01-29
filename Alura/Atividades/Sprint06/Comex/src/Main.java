fa0b2645b2926b7b31ca9a8eb9acf678b33e0e12
thaizerbinatti <thatazerb@gmail.com>
2024-01-29 17:56:55 -0300

    sprint 6

--- Alura/Atividades/Sprint06/Comex/src/Main.java
+++ Alura/Atividades/Sprint06/Comex/src/Main.java
@@ -30,8 +30,66 @@ public class Main {
 
         // Atividade 5
 
+        double valorLivro = 49.95;
+        double valorCelular = 1470.76;
+        System.out.println("Cadastre um produto e o valor");
+        System.out.println("_______________________________________");
+        System.out.println("Nome: Livro");
+        System.out.println("Preço: R$" + valorLivro);
+        System.out.println("_______________________________________");
+        System.out.println("Nome: Celular");
+        System.out.println("Preço: R$" + valorCelular);
+        System.out.println("_______________________________________");
+
+
+        // Atividade 7
+        int anoAtual = 2023;
+        int nascimentoCliente1 = 1980;
+        int nascimentoCliente2 = 2000;
+        int idadeCliente1 = anoAtual - nascimentoCliente1;
+        int idadeCliente2 = anoAtual - nascimentoCliente2;
+
+
+        // System.out.println(">>> Listagem do Cliente");
+        // System.out.println(">>> Ano Atual: " + anoAtual);
+        // System.out.println("_______________________________________");
+        // System.out.println("Nome: Fulano");
+        // System.out.println("Data de nascimento: " + nascimentoCliente1);
+        // System.out.println("Idade: " + idadeCliente1);
+        // System.out.println("Caracteristica Etária: " + classificacao1);
+        // System.out.println("_______________________________________");
+        // System.out.println("Nome: Beltrano");
+        // System.out.println("Data de nascimento: " + nascimentoCliente2);
+        // System.out.println("Idade: " + idadeCliente2);
+        // System.out.println("Caracteristica Etária: " + classificacao2);
+
+        String classificacao1;
+        if (idadeCliente1 <= 17) {
+            classificacao1 = "ADOLESCENTE";
+        } else if (idadeCliente1 <= 29) {
+            classificacao1 = "JOVEM";
+        } else if (idadeCliente1 <= 59) {
+            classificacao1 = "ADULTO";
+        } else {
+            classificacao1 = "IDOSO";
+        }
+
+        String classificacao2;
+        if (idadeCliente2 <= 17) {
+            classificacao2 = "ADOLESCENTE";
+        } else if (idadeCliente2 <= 29) {
+            classificacao2 = "JOVEM";
+        } else if (idadeCliente2 <= 59) {
+            classificacao2 = "ADULTO";
+        } else {
+            classificacao2 = "IDOSO";
+        }
+
+
+       // Atividade 5 com while
         int sair = 1;
         while (sair != 0) {
+            System.out.println("Atividade 5 feita com while durante a aula");
             System.out.println("Cadastre um produto e o valor");
             String produto = leitura.next();
             double valor = leitura.nextDouble();
---
+++ Studies/Anotacoes/Java/anotacoes.java
@@ -0,0 +1,16 @@
+public class DesafioTemperatura {
+
+    public static void main(String[] args) {
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+
+    }
+
+}

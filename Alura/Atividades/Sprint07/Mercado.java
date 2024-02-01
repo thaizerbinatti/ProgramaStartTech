8e02872e9dcd27e17e5fcc6921b9e7acfb6d3e40
thaizerbinatti <thatazerb@gmail.com>
2024-02-01 16:37:15 -0300

    print7

---
+++ Alura/Atividades/Sprint07/Mercado.java
@@ -0,0 +1,15 @@
+public class Mercado {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto();
+        produto1.nome = "Detergente";
+        produto1.descricao = "Produto usado para lavar louças";
+        produto1.precoUnitario = 2.99;
+        produto1.quantidade = 8;
+
+        Produto produto2 = new Produto();
+        produto2.nome = "Sabão em pó";
+        produto2.descricao = "Usado para lavar roupas";
+        produto2.precoUnitario = 23.99;
+        produto2.quantidade = 6;
+    }
+}
---
+++ Alura/Atividades/Sprint07/Produto.java
@@ -0,0 +1,6 @@
+public class Produto {
+    String nome;
+    String descricao;
+    double precoUnitario;
+    int quantidade;
+}
